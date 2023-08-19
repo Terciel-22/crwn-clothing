8789ca737e6660f6433830773446376136819d5d
terfiel-22 <dietherfiel@gmail.com>
2023-08-19 12:58:27 +0800

    Add to Cart part 1

--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -1,12 +1,25 @@
 import { createContext, useState } from "react";
 
+const addCartItem = (cartItems, productToAdd) => {
+  // find if cartItems contains productToAdd
+  // if found, increment quantity.
+  // return new arraywith modified cartItems.
+};
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
 });
 
 export const CartProvider = ({ children }) => {
-  const [isCartOpen, setIsCartOpen] = useState();
-  const value = { isCartOpen, setIsCartOpen };
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+
+  const addItemToCart = (productToAdd) =>
+    setCartItems(addCartItem(cartItems, productToAdd));
+
+  const value = { isCartOpen, setIsCartOpen, addItemToCart };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
