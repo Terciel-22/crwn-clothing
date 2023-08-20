584286346c4c06fbb5775d4f3eea2ab923cdbbef
terfiel-22 <dietherfiel@gmail.com>
2023-08-20 15:08:05 +0800

    Remove Cart item in Checkout page

--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -32,12 +32,16 @@ const removeCartItem = (cartItems, cartItemToRemove) => {
   }
 };
 
+const clearCartItem = (cartItems, cartItemToClear) =>
+  cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
   removeItemToCart: () => {},
+  clearItemFromCart: () => {},
   cartCount: 0,
 });
 
@@ -59,12 +63,16 @@ export const CartProvider = ({ children }) => {
   const removeItemToCart = (cartItemToRemove) => {
     setCartItems(removeCartItem(cartItems, cartItemToRemove));
   };
+  const clearItemFromCart = (cartItemToClear) => {
+    setCartItems(clearCartItem(cartItems, cartItemToClear));
+  };
 
   const value = {
     isCartOpen,
     setIsCartOpen,
     addItemToCart,
     removeItemToCart,
+    clearItemFromCart,
     cartItems,
     cartCount,
   };
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,41 +1,33 @@
 import { useContext } from "react";
 import "./checkout.styles.scss";
 import { CartContext } from "../../context/cart.context";
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
 
 const Checkout = () => {
-  const { cartItems, addItemToCart, removeItemToCart } =
-    useContext(CartContext);
+  const { cartItems } = useContext(CartContext);
   return (
-    <div>
-      <h1>I'm the Checkout Page</h1>
-      <div>
-        {cartItems.map((cartItem) => {
-          const { id, name, price, quantity } = cartItem;
-          return (
-            <div key={id}>
-              <h2>{name}</h2>
-              <span>{quantity}</span> x <span>{price}</span>
-              <br />
-              <span
-                onClick={() => {
-                  removeItemToCart(cartItem);
-                }}
-              >
-                decrement
-              </span>
-              <br />
-              <span
-                onClick={() => {
-                  addItemToCart(cartItem);
-                }}
-              >
-                increment
-              </span>
-              <br />
-            </div>
-          );
-        })}
+    <div className="checkout-container">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>Product</span>
+        </div>
+        <div className="header-block">
+          <span>Description</span>
+        </div>
+        <div className="header-block">
+          <span>Quantity</span>
+        </div>
+        <div className="header-block">
+          <span>Price</span>
+        </div>
+        <div className="header-block">
+          <span>Remove</span>
+        </div>
       </div>
+      {cartItems.map((cartItem) => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
+      <span className="total">Total: 0</span>
     </div>
   );
 };
