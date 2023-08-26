cdd14cbeaa4b7d24459e21282b21116bcaf6b380
terfiel-22 <dietherfiel@gmail.com>
2023-08-26 15:25:21 +0800

    categoriesReducer

--- src/context/categories.context.jsx
+++ src/context/categories.context.jsx
@@ -1,12 +1,43 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
 });
 
+export const CATEGORY_ACTION_TYPE = {
+  SET_CATEGORY_MAP: "SET_CATEGORY_MAP",
+};
+
+const categoriesReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case CATEGORY_ACTION_TYPE.SET_CATEGORY_MAP:
+      return {
+        ...state,
+        categoriesMap: payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+const INITIAL_CATEGORIES_MAP = {
+  categoriesMap: {},
+};
 export const CategoriesProvider = ({ children }) => {
-  const [categoriesMap, setCategoriesMap] = useState({});
+  const [{ categoriesMap }, dispatch] = useReducer(
+    categoriesReducer,
+    INITIAL_CATEGORIES_MAP
+  );
+  const setCategoriesMap = (categoryMap) => {
+    dispatch({
+      type: CATEGORY_ACTION_TYPE.SET_CATEGORY_MAP,
+      payload: categoryMap,
+    });
+  };
 
   useEffect(() => {
     const getCategoriesMap = async () => {
